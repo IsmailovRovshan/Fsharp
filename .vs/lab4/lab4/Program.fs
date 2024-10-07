33af193eab27c2841714195fa4046efb33f1739f
Ровшан Исмаилов <roma.ismailov96@mail.ru>
2024-10-08 00:27:53 +0400

    the end

--- .vs/lab4/lab4/Program.fs
+++ .vs/lab4/lab4/Program.fs
@@ -1,2 +1,64 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿//  Создать иерархию классов и интерфейсов для предметной области 
+//  (класса 3 – 4, из них хотя бы один класс должен являться наследником другого класса), 
+//  заданной номером варианта, и продемонстрировать её работу
+
+// Определение интерфейса банковских операций
+type IBankOperation =
+    abstract member PerformOperation: unit -> unit
+
+// Базовый класс "Человек"
+type Person(name: string, age: int) =
+    member this.Name = name
+    member this.Age = age
+    override this.ToString() = sprintf "%s, возраст: %d" name age
+
+// Класс "Клиент", наследуемый от "Человек"
+type Client(name: string, age: int, clientId: string) =
+    inherit Person(name, age)
+    member val ClientId = clientId with get
+    override this.ToString() = sprintf "Клиент: %s, ID: %s" (base.ToString()) clientId
+
+// Класс "Банковский счет"
+type BankAccount(accountNumber: string, balance: decimal) =
+    member val AccountNumber = accountNumber with get
+    member val Balance = balance with get, set
+    member this.Deposit(amount: decimal) =
+        this.Balance <- this.Balance + amount
+    member this.Withdraw(amount: decimal) =
+        if this.Balance >= amount then
+            this.Balance <- this.Balance - amount
+        else
+            printfn "Недостаточно средств на счете"
+    override this.ToString() = sprintf "Счет %s, Баланс: %M" accountNumber balance
+
+// Класс "Депозит", реализующий интерфейс IBankOperation
+type Deposit(account: BankAccount, amount: decimal) =
+    interface IBankOperation with
+        member this.PerformOperation() =
+            account.Deposit(amount)
+            printfn "Депозит на сумму %M выполнен. Новый баланс: %M" amount account.Balance
+
+// Класс "Снятие", реализующий интерфейс IBankOperation
+type Withdrawal(account: BankAccount, amount: decimal) =
+    interface IBankOperation with
+        member this.PerformOperation() =
+            account.Withdraw(amount)
+            printfn "Снятие на сумму %M выполнено. Новый баланс: %M" amount account.Balance
+
+// Класс "Менеджер", наследуемый от "Человек"
+type Manager(name: string, age: int, employeeId: string) =
+    inherit Person(name, age)
+    member val EmployeeId = employeeId with get
+    member this.PerformOperation(operation: IBankOperation) =
+        operation.PerformOperation()
+    override this.ToString() = sprintf "Менеджер: %s, ID: %s" (base.ToString()) employeeId
+
+// Пример использования
+let client = Client("Алексей Иванов", 34, "CL12345")
+let account = BankAccount("ACC123", 1000M)
+let manager = Manager("Мария Петрова", 29, "EMP987")
+
+printfn "%s" (client.ToString())
+printfn "%s" (account.ToString())
+manager.PerformOperation(Deposit(account, 500M))
+manager.PerformOperation(Withdrawal(account, 200M))
---
+++ .vs/lab4/lab5/Program.fs
@@ -0,0 +1,30 @@
+﻿open System.IO
+open System.Threading.Tasks
+
+// Функция для изменения порядка слов в строке
+let reverseWords (line: string) =
+    line.Split(' ')
+    |> Array.rev
+    |> String.concat " "
+
+// Обработка одного файла: изменение порядка слов в каждой строке
+let processFile (filePath: string) =
+    let lines = File.ReadAllLines(filePath)
+    let reversedLines = lines |> Array.map reverseWords
+    reversedLines
+
+// Основная функция, выполняющая параллельную обработку файлов
+let processFiles (inputFiles: string list) (outputFile: string) =
+    let resultLock = obj() // Для синхронизации записи в выходной файл
+    Parallel.ForEach(inputFiles, fun file ->
+        let reversedLines = processFile(file)
+        lock resultLock (fun () ->
+            File.AppendAllLines(outputFile, reversedLines)
+        )
+    )
+
+// Пример использования
+let inputFiles = ["file1.txt"; "file2.txt"; "file3.txt"] // Пути к входным файлам
+let outputFile = "output.txt" // Путь к выходному файлу
+
+processFiles inputFiles outputFile
