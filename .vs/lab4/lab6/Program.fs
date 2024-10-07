2a55bb210043481ce4009ce47ca2f719193bef14
Ровшан Исмаилов <roma.ismailov96@mail.ru>
2024-10-08 00:43:40 +0400

    the end

---
+++ .vs/lab4/lab6/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+// Функция для применения правил L-системы
+let rec applyRules (input: string) : string =
+    input.Replace("F", "F-F-F-F-F") // Правило: F -> F-F-F-F-F
+
+// Функция для генерации L-системы на основе заданного количества итераций
+let rec generateLSystem (axiom: string) (iterations: int) : string =
+    if iterations = 0 then
+        axiom
+    else
+        generateLSystem (applyRules axiom) (iterations - 1)
+
+// Вывод результата L-системы для заданного количества итераций
+let lSystemResult = generateLSystem "-FF+FF+FF-" 2 // Пример: 2 итерации
+printfn "%s" lSystemResult
+
